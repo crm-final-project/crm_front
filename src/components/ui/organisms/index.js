bd8c89c650e903f8ea6f09bb7d1d82a271bff012
Jhonathan Alejandro <jhonathanalejandroa@gmail.com>
2022-04-11 23:50:59 -0500

    update export organisms

--- src/components/ui/organisms/index.js
+++ src/components/ui/organisms/index.js
@@ -2,3 +2,4 @@ export { MyQuotesOrg } from './MyQuotesOrg';
 export { UserLogin } from './UserLogin';
 export { ValidateEmail } from './ValidateEmail';
 export { UserRegForm } from './UserRegForm';
+export { UserEditForm } from './UserEditForm';
