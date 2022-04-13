b6fb31e1ce75a71fa38c09ea6fe82cefb7336768
Wilder Rincon <1588@holbertonschool.com>
2022-04-13 18:52:07 -0500

    84 - Create landing page project

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 import {
   LandingPage,
   LoginPage,
@@ -7,17 +9,43 @@ import {
   UserRegPage,
   ValidateEmailPage,
 } from '../components/pages';
+import { makeStyles } from '@mui/styles';
+import Loader from '../components/helper/Loader';
+
+const useStyles = makeStyles((theme) => ({
+  loader : {
+    opacity: 0
+  },
+  loaderActive: {
+    opacity: 100
+  }
+}));
 
 const AppRouter = () => {
+  const [loading, setLoading] = useState(true);
+  const classes = useStyles();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
   return (
-    <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<UserRegPage />} />
-      <Route path='/myquotes' element={<MyQuotesPage />} />
-      <Route path='/validate' element={<ValidateEmailPage/>}/>
-      <Route path='*' element={<Four0Four />} />
-    </Routes>
+    <>
+      {loading && 
+        <Box sx={{display: 'flex', height: '100vh'}}>
+          <Loader sx={{opacity: 100}}/>
+        </Box>
+      }
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<UserRegPage />} />
+        <Route path='/myquotes' element={<MyQuotesPage />} />
+        <Route path='/validate' element={<ValidateEmailPage/>}/>
+        <Route path='*' element={<Four0Four />} />
+      </Routes>
+    </>
   );
 };
 
