9acffa4015d56c1652cfa37565d6f8dfd62f1246
ronniebm <ronnie.coding@gmail.com>
2022-04-17 05:10:17 -0500

    get quotations request was added

--- src/components/ui/organisms/QuotesOrg/QuotesOrg.jsx
+++ src/components/ui/organisms/QuotesOrg/QuotesOrg.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, Container } from '@mui/material';
 import { DATA } from './quotesdata';
 import { TotalCash } from '../../atoms';
@@ -28,19 +28,38 @@ export const QuotesOrg = (props) => {
     showModal: {
       status: showModal,
       setter: setShowModal,
-    }
+    },
   };
+
+  const getQuotes = () => {
+    fetch("http://localhost:3001/quotations")
+      .then((resp) => resp.json())
+      .then(function (data) {
+        console.log(data)
+        });
+  };
+
+  useEffect(() => getQuotes, [])
 
   return (
     <Box className='my-quotes-org-box'>
       <Box className='my-quotes-org-box__list'>
         <HeaderBar pageTitle='My Quotes' />
         <FlowOptions btn1_title='Get report' btn2_title='New Quote' />
-        <Container >
+        <Container>
           <TotalCash val={cashVal} className='quote-detail-r-box__total-list' />
         </Container>
-        <TabsBarR states={states} data={activeTab ? DATA.month : DATA.older} val1={DATA.month.length} val2={DATA.older.length} />
-        <ListR states={states} data={activeTab ? DATA.month : DATA.older} view={props.view} />
+        <TabsBarR
+          states={states}
+          data={activeTab ? DATA.month : DATA.older}
+          val1={DATA.month.length}
+          val2={DATA.older.length}
+        />
+        <ListR
+          states={states}
+          data={activeTab ? DATA.month : DATA.older}
+          view={props.view}
+        />
       </Box>
 
       <Box className='my-quotes-org__detail'>
