acab2e6e16f50184f02f4ac1303291fcf3762759
Jose A Vallejo <jose.vallejo@globant.com>
2022-04-27 21:01:10 -0500

    implement Outlet component to nested routes

--- src/routes/public/router.js
+++ src/routes/public/router.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation, Outlet } from 'react-router-dom';
 import { Box } from '@mui/material';
 import {
   LandingPage,
@@ -7,9 +7,14 @@ import {
   UserRegPage,
   RecoveryAccountPage,
   ValidateEmailPage,
+  Four0Four,
+  QuotesNewPage,
+  UserEditPage,
+  QuotesPage,
 } from '../../components/pages';
+
 import Loader from '../../components/helper/Loader';
-import { QuotesPage } from '../../components/pages';
+
 import { Loggin } from '../../utils/login';
 
 const loggin = new Loggin();
@@ -31,11 +36,14 @@ const PublicRouter = () => {
       )}
       <Routes>
         <Route path='/' element={<LandingPage />} />
-        <Route path='/quotes' element={
-          <RequireAuth redirectTo="/login">
-            <QuotesPage />
-          </RequireAuth>
-        } />
+        <Route element={<RequireAuth />}>
+          <Route path='/quotes' element={<QuotesPage />} />
+          <Route path='/new' element={<QuotesNewPage />} />
+          <Route path='/validate' element={<ValidateEmailPage />} />
+          <Route path='/edit' element={<UserEditPage />} />
+          <Route path='/reset-password/:userId' element={<ValidateEmailPage />} />
+          <Route path='*' element={<Four0Four />} />
+        </Route>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<UserRegPage />} />
         <Route path='/forgot' element={<RecoveryAccountPage />} />
@@ -46,12 +54,19 @@ const PublicRouter = () => {
   );
 };
 
+function RequireAuth() {
+  let auth = loggin.isLoggedIn();;
+  let location = useLocation();
 
+  if (!auth) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
 
-function RequireAuth({ children, redirectTo }) {
-  let isAuthenticated = loggin.isLoggedIn();
-  return isAuthenticated ? children : <Navigate to={redirectTo} />;
+  return <Outlet />;
 }
-
 
 export default PublicRouter;
