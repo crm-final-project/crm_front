b4e5958a888839a9235c8403243013bb9f6a193d
Jose A Vallejo <jose.vallejo@globant.com>
2022-04-30 09:30:00 -0500

    add gh-pages

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpRequest = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://ec2-54-167-58-155.compute-1.amazonaws.com', //'http://localhost:8000',
   timeout: 1000,
   withCredentials: true,
 });
--- src/environments/index.js
+++ src/environments/index.js
@@ -1,11 +1,11 @@
 
-const dotenv = require("dotenv")
+// const dotenv = require("dotenv")
 
-dotenv.config({ path: './development.env' })
+// dotenv.config({ path: './development.env' })
 
 const environments = () => {
   if (process.env.NODE_ENV !== 'production') {
-    dotenv.config({ path: './development.env' })
+    // dotenv.config({ path: './development.env' })
   }
 }
 module.exports = environments
