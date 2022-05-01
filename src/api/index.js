99a794d5d88812beb08286494397b561ad12d8a6
Jose A Vallejo <jose.vallejo@globant.com>
2022-05-01 09:49:29 -0500

    using ngrok en the backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
 const httpRequest = axios.create({
-  baseURL: 'http://ec2-54-167-58-155.compute-1.amazonaws.com', //'http://localhost:8000',
+  baseURL: 'https://da92-54-167-58-155.ngrok.io', //'http://localhost:8000',
   timeout: 1000,
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'allow-origin': '*',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization, set-cookie',
+  },
 });
 
 export default httpRequest;
